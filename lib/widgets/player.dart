86f3bb645456ad5c385900a831005be6155b305b
petlyh <88139840+petlyh@users.noreply.github.com>
2023-11-30 17:50:41 +0100

    Add playback speed selector

--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -35,8 +35,43 @@ class PlayerWidget extends ConsumerWidget {
                     : controller.play(),
                 child: VideoPlayer(controller)),
           ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: SpeedWidget(controller.setPlaybackSpeed),
+          ),
         ],
       ),
     );
+  }
+}
+
+final speedProvider = StateProvider.autoDispose((_) => 1.0);
+
+class SpeedWidget extends ConsumerWidget {
+  const SpeedWidget(this.onChanged, {super.key});
+
+  final void Function(double speed) onChanged;
+
+  static const speedValues = [0.25, 0.5, 1.0];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final speed = ref.watch(speedProvider);
+
+    return SegmentedButton(
+        style: const ButtonStyle(
+          padding:
+              MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 12)),
+        ),
+        segments: speedValues
+            .map((value) =>
+                ButtonSegment(value: value, label: Text(value.toString())))
+            .toList(),
+        selected: {speed},
+        onSelectionChanged: (selected) {
+          final value = selected.first;
+          ref.read(speedProvider.notifier).state = value;
+          onChanged(value);
+        });
   }
 }
